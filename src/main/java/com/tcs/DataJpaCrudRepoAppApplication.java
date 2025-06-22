307667a98f78057fd56987bfece7a6d996196d47
Hemant <hk9934867349@gmail.com>
2025-06-22 12:19:22 +0530

    modified code

--- src/main/java/com/tcs/DataJpaCrudRepoAppApplication.java
+++ src/main/java/com/tcs/DataJpaCrudRepoAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -16,16 +18,51 @@ public class DataJpaCrudRepoAppApplication {
 		System.out.println("Hello");
 		ConfigurableApplicationContext ctxt =SpringApplication.run(DataJpaCrudRepoAppApplication.class, args);
 		BookRepository repo = ctxt.getBean(BookRepository.class);
-		/*Book b = new Book();
-		b.setBookId(101);
-		b.setBookName("Spring");
-		b.setBookPrice(1000.00);
+		/*Book b1 = new Book();
+		b1.setBookId(102);
+		b1.setBookName("Springboot");
+		b1.setBookPrice(3000.00);
 
-		repo.save(b);
-		System.out.println("Record inserted");*/
+		Book b2 = new Book();
+		b2.setBookId(103);
+		b2.setBookName("Java");
+		b2.setBookPrice(2000.00);*/
 
-		Optional<Book> findById =repo.findById(101);
-		System.out.println(findById.get());
+		//repo.saveAll(Arrays.asList(b1,b2));
+		//boolean s = repo.existsById(105);
+		//System.out.println("Record count::"+repo.count());
+		//System.out.println("Record inserted");
+		/*Optional<Book> findById = repo.findById(103);
+		if(findById.isPresent()){
+			System.out.println(findById.get());
+		}*/
+       /*Iterable<Book> findAllById=repo.findAllById(Arrays.asList(101,102,103));
+	   for(Book b : findAllById){
+		   System.out.println(b);
+	   }*/
+		/*Iterable<Book> findAll = repo.findAll();
+		for(Book b : findAll){
+			System.out.println(b);
+		}*/
+		/*if(repo.existsById(102)){
+			repo.deleteById(102);
+		}
+		else{
+			System.out.println("record not found");
+		}*/
+
+		/*List<Book> books =repo.findByBookPriceGreaterThan(1500.00);
+		for(Book b : books){
+			System.out.println(b);
+		}*/
+		/*List<Book> books =repo.AllBooks();
+		for(Book b : books){
+			System.out.println(b);
+		}*/
+		List<Book> books =repo.getBooks();
+		for(Book b : books){
+			System.out.println(b);
+		}
 	}
 
 }
--- src/main/java/com/tcs/repo/BookRepository.java
+++ src/main/java/com/tcs/repo/BookRepository.java
@@ -1,8 +1,20 @@
 package com.tcs.repo;
 import com.tcs.entity.Book;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    public List<Book> findByBookPriceGreaterThan(Double price);
+    public List<Book> findByBookName(String bookName);
+
+    @Query(value="select * from book", nativeQuery = true)
+    public List<Book> AllBooks();
+
+    @Query("from Book")
+    public List<Book> getBooks();
 
 }
